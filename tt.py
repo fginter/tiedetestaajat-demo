52ed66fc4c67d0afa31fbd96b9ccf59c31e7fd57
Filip Ginter <filip.ginter@gmail.com>
2021-04-06 15:42:44 +0300

    filter

--- tt.py
+++ tt.py
@@ -2,7 +2,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import flask
 from flask import Flask
 from flask import render_template, request
-
+import re
 from happytransformer import HappyWordPrediction
     
 app = Flask(__name__)
@@ -11,6 +11,14 @@ APP_ROOT = "/tt"
 app.config["APPLICATION_ROOT"] = APP_ROOT
 
 happy_wp = HappyWordPrediction("BERT", "TurkuNLP/bert-base-finnish-cased-v1")
+
+
+with open("kirosanat.txt") as f:
+    re_parts=[]
+    for line in f:
+        line=line.strip()
+        re_parts.append(f"({line})")
+    kiro_re=re.compile("|".join(re_parts))
 
 @app.route("/")
 def index():
@@ -22,7 +30,8 @@ def predict():
     inpsentence=request.json["sentencein"].replace("SANA","[MASK]",1)
     if "[MASK]" not in inpsentence:
         return {"predictions_html":"Et muistanut laittaa yksi SANA."}
-    predictions = happy_wp.predict_mask(inpsentence,top_k=5)
+    predictions = happy_wp.predict_mask(inpsentence,top_k=10)
+    predictions = [p for p in predictions if not kiro_re.match(p.token)]
     print(list(predictions))
     predictions_html=render_template("result.html",predictions=predictions)
     return {"predictions_html":predictions_html}
